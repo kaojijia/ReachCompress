9faed2e87da3e9cf8bc321e6c84f887848f32e3c
kaojijia <kaojijia@qq.com>
2024-12-15 12:12:09 +0800

    complete Bloom filter

--- include/BloomFilter.h
+++ include/BloomFilter.h
@@ -1,18 +1,26 @@
-
 #ifndef BLOOM_FILTER_H
 #define BLOOM_FILTER_H
 
 #include "graph.h"
 #include "Algorithm.h"
 #include <vector>
+#include <bitset>
 
 class BloomFilter : public Algorithm {
 public:
-    // BloomFilter(Graph& graph);
-    // void offline_industry();
-    // bool reachability_query(int source, int target);
+    BloomFilter(Graph& graph);
+    void offline_industry() override;
+    bool reachability_query(int source, int target) override;
+    double false_positive_rate(int vertex); // 计算某个顶点的假阳性率
+    std::unordered_map<std::string, size_t> getIndexSizes() const override; // 计算索引大小
+
 private:
-    // std::vector<bool> bitArray_;
+    Graph& graph;
+    std::vector<std::bitset<64>> filters;
+    size_t numHashFunctions;
+    std::vector<size_t> insertedElements; // 记录每个顶点插入的元素数量
+
+    std::vector<size_t> generateHashes(int key) const;
 };
 
 #endif // BLOOM_FILTER_H
--- test/test_bloom.cpp
+++ test/test_bloom.cpp
@@ -1,19 +1,19 @@
 #include "gtest/gtest.h"
 #include "graph.h"
-#include "TreeCover.h"
+#include "BloomFilter.h"
 #include <algorithm>  // 确保包含算法库
 #include "utils/InputHandler.h"
 #include "utils/OutputHandler.h"
 
-TEST(TreeCover, test_tree_cover) {
+TEST(BloomFilter, test_BLOOM) {
     Graph g(false);  // 确保存储边集    
     InputHandler inputHandler(PROJECT_ROOT_DIR"/Edges/generate/gene_edges_20241029_135003");
     inputHandler.readGraph(g);
     OutputHandler::printGraphInfo(g);
-    TreeCover treecover(g);
-    treecover.offline_industry();
-    treecover.printIndex();
-    EXPECT_TRUE(treecover.reachability_query(0, 5));
-    EXPECT_TRUE(treecover.reachability_query(4, 5));
-    EXPECT_FALSE(treecover.reachability_query(4, 3));
+    BloomFilter bloom(g);
+    bloom.offline_industry();
+    bloom.reachability_query(0,1);
+    EXPECT_TRUE(bloom.reachability_query(19, 15));
+    EXPECT_FALSE(bloom.reachability_query(15, 19));
+    EXPECT_FALSE(bloom.reachability_query(8, 0));
 }
